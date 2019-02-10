18accde58d87bdeb1bf85f4d83ae569d9f0f0730
ko kristin <koberlander115@gmail.com>
2019-02-10 21:05:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,9 +23,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-# monopoly = {:railroads => {:pieces => 4, :names => {}}}
-# monopoly[:railroads][:names] = {Hash.new(0)}
-# monopoly[:railroads][:names] = {}
+  monopoly = {:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {}}}
 	
 end
 
